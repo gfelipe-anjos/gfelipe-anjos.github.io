6ed33dc9fae0b78702d1ea58b4b89460080b4c92
PedroLara-jpg <mrpedro342@gmail.com>
2025-11-25 10:57:12 -0300

    aula 25/11

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,3 +1,6 @@
+const themeToggle = document.getElementById('themeToggle');
+const body = document.body;
+const themeIcon = document.querySelector('.theme-icon');
 const typed = new Typed(".multiple-text",{
     strings: [
         "preguisoso",
@@ -9,3 +12,35 @@ const typed = new Typed(".multiple-text",{
     backDelay:1000,
     loop:true,
 })
+function toggleTheme() {
+    body.classList.toggle('light-mode');
+    const isLightMode = body.classList.contains('light-mode');
+
+    if (isLightMode) {
+        themeIcon.className = 'bx bx-moon theme-icon';
+        localStorage.setItem('theme', 'light');
+    } else{
+        themeIcon.className = 'bx bx-sun theme-icon';
+        localStorage.setItem('theme', 'dark');
+    }
+}    
+
+themeToggle.addEventListener('click',toggleTheme);
+
+function loadSavedTheme(){
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'light'){
+        body.classList.add('light-mode');
+        themeIcon.className = 'bx bx-moon theme-icon';
+    } else{
+        body.classList.remove('light-mode');
+        themeIcon.className = 'bx bx-sun theme-icon';
+    }
+}
+
+if (document.readyState === 'loading'){
+    document.addEventListener('DOMContentLoded', loadSavedTheme);
+}else{
+    loadSavedTheme();
+}
